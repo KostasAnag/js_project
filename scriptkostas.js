89cb4cd5d42ad820e0055bd27c1155ce90dba7a3
KostasAnag <anagnokostas@hotmail.com>
2022-07-21 19:01:17 +0300

    first commit

---
+++ scriptkostas.js
@@ -0,0 +1,53 @@
+
+//------------------    COUNTDOWN TIMER   ------------------------//
+
+// Set the date we're counting down to
+	var countDownDate =  new Date("Nov 5, 2023 15:37:25").getTime();
+
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+
+		 // Get today's date and time
+		var now = new Date().getTime();
+
+		// Find the distance between now and the count down date
+		var distance = countDownDate - now;
+
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		//document.getElementById("days").innerHTML = days + "</br>days";
+		//document.getElementById("hours").innerHTML = hours + "</br>hours";
+		//document.getElementById("minutes").innerHTML = minutes + "</br>minutes";
+		//document.getElementById("seconds").innerHTML = seconds + "</br>seconds";
+		document.getElementById("counter").innerHTML = days + "d |" + hours + "h | "
+	+ minutes + "m |" +" "+ seconds + "s ";
+    
+
+	  
+	  // If the count down is over, write some text 
+		if (distance < 0) {
+		clearInterval(x);
+		document.getElementById("demo").innerHTML = "EXPIRED";
+		  }
+		}, 1000);
+		
+//-----------------HIDE INPUT FIELD IN CONTACT ME MODAL--------------------------		
+
+ function appear(){
+		
+		document.getElementById("child").classList.toggle("visually-hidden");
+	}
+	 
+	 /*function appear(){
+		if(document.getElementById('children').checked === true){
+			document.getElementById("child").style.display="block";
+		} else{
+		document.getElementById("child").style.display="none";
+		}
+	  
+	  }*/
+		
+		
